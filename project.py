bf3f25165f98431fb8f100c5cd91a6bacaf2fc24
ayushgaps-09 <ayushg0905@gmail.com>
2026-04-06 02:18:22 +0530

    Update project.py

--- project.py
+++ project.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("☁️ CLOUD STORAGE GROWTH MODEL")
+st.title("d CLOUD STORAGE GROWTH MODEL")
 st.markdown("### Exponential vs Logistic vs Hybrid Growth Models")
 
 # ---------------- SIDEBAR ----------------
